620d78617bc8c7b4126d1f3eacd7fdd5507f0de4
Jesse Hu <huyouare@gmail.com>
2014-06-27 18:14:38 -0700

    Edit url parse

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var url = require('url');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -30,7 +31,7 @@ app.get('/api', function (req, res) {
 });
 
 app.get('/yo-up', function (req, res) {
-  var url_parts = url.parse(request.url, true);
+  var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
   
   socket = {};
