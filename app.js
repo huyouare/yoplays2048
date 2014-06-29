b77f127b2a693e4031b2970b438e0e8d1d6db825
Jesse Hu <huyouare@gmail.com>
2014-06-29 02:09:01 -0700

    Missing bracket

--- app.js
+++ app.js
@@ -353,7 +353,7 @@ io.sockets.on('connection', function (socket) {
   //       });
   //     }
   //   }
-  });
+  // });
 
   socket.on('disconnect', function () {
     io.sockets.emit('someone disconnected', {
